82fd37d7e25c45a189e2581df4ae6bca1101752d
Akif Eren <125217594+1lord0@users.noreply.github.com>
2025-06-27 21:24:15 +0300

    Update new.py

--- new.py
+++ new.py
@@ -8,6 +8,7 @@ st.title("📊 Öğrenci Not ve Katılım Takip Uygulaması")
 
 # 1. CSV dosyası yükle
 uploaded_file = st.file_uploader("CSV dosyasını yükleyin", type=["csv"], key="file1")
+df.columns = df.columns.str.strip().str.lower()  # sütunları düzelt
 
 if uploaded_file is not None:
     # 2. Veriyi oku ve temizle
